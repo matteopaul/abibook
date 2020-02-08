e5712aa01dbf0c241663d54e7620c92f3d10fc4e
Matteo Illing <matteo.illing@web.de>
2020-02-08 14:39:50 +0100

    https

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const sassMiddleware = require('node-sass-middleware');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 var path = require('path');
+var fs = require('fs');
 
-const app = express();
+var privateKey = fs.readFileSync('sslcert/server.key');
+var certificate = fs.readFileSync('sslcert/server.crt');
+
+var credentials = {key: privateKey, cert: certificate};
+
+const app = express(credentials);
 
 const database = mysql.createConnection({
   host: "localhost",
